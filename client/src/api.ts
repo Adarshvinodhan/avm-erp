3833b11ac3c5292b95d4aa01729ea8cb5af91bdd
adarsh <adarshvinodhan@gmail.com>
2025-04-07 17:52:34 +0530

    mobile nav fix

---
+++ client/src/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+  // baseURL: 'http://localhost:3000', 
+  baseURL:'https://avm-erp.onrender.com/'
+
+});
+
+// Add a request interceptor to dynamically set the Authorization header
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // Handle errors during request setup
+    return Promise.reject(error);
+  }
+);
+
+export default api;
