ebf07464d1804dc1ae1e64c29bddaff0a7895729
Adarshvinodhan <100936187+Adarshvinodhan@users.noreply.github.com>
2025-04-20 02:51:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "/client/dist")));
 app.use(cors());
 app.use(express.json());    
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
